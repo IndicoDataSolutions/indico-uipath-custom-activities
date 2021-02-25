4c5db27eb4ac98be33495a7627560d8311111f77
Rafal Kaczka <rafalkaczka@venturedevs.com>
2021-02-25 15:56:22 +0100

    INT-59 fix classify

--- Indico.RPAActivities/Indico.RPAActivities.Activities/Activities/Classify.cs
+++ Indico.RPAActivities/Indico.RPAActivities.Activities/Activities/Classify.cs
@@ -1,34 +1,23 @@
 using System;
 using System.Activities;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Indico.RPAActivities.Activities.Activities;
 using Indico.RPAActivities.Activities.Properties;
 using UiPath.Shared.Activities;
 using UiPath.Shared.Activities.Localization;
-using UiPath.Shared.Activities.Utilities;
 
 namespace Indico.RPAActivities.Activities
 {
     [LocalizedDisplayName(nameof(Resources.Classify_DisplayName))]
     [LocalizedDescription(nameof(Resources.Classify_Description))]
-    public class Classify : ContinuableAsyncCodeActivity
+    public class Classify : IndicoActivityBase<
+        (List<string> Text, int ModelGroup),
+        (List<string> TopResults, List<Dictionary<string, double>> Results)
+    >
     {
-        #region Properties
-
-        /// <summary>
-        /// If set, continue executing the remaining activities even if the current activity has failed.
-        /// </summary>
-        [LocalizedCategory(nameof(Resources.Common_Category))]
-        [LocalizedDisplayName(nameof(Resources.ContinueOnError_DisplayName))]
-        [LocalizedDescription(nameof(Resources.ContinueOnError_Description))]
-        public override InArgument<bool> ContinueOnError { get; set; }
-
-        [LocalizedCategory(nameof(Resources.Common_Category))]
-        [LocalizedDisplayName(nameof(Resources.Timeout_DisplayName))]
-        [LocalizedDescription(nameof(Resources.Timeout_Description))]
-        public InArgument<int> TimeoutMS { get; set; } = 60000;
-
         [LocalizedDisplayName(nameof(Resources.Classify_Text_DisplayName))]
         [LocalizedDescription(nameof(Resources.Classify_Text_Description))]
         [LocalizedCategory(nameof(Resources.Input_Category))]
@@ -49,20 +38,12 @@ namespace Indico.RPAActivities.Activities
         [LocalizedCategory(nameof(Resources.Output_Category))]
         public OutArgument<List<Dictionary<string, double>>> Results { get; set; }
 
-        #endregion
-
-
-        #region Constructors
 
         public Classify()
         {
             Constraints.Add(ActivityConstraints.HasParentType<Classify, IndicoScope>(string.Format(Resources.ValidationScope_Error, Resources.IndicoScope_DisplayName)));
         }
 
-        #endregion
-
-
-        #region Protected Methods
 
         protected override void CacheMetadata(CodeActivityMetadata metadata)
         {
@@ -72,50 +53,36 @@ namespace Indico.RPAActivities.Activities
             base.CacheMetadata(metadata);
         }
 
-        protected override async Task<Action<AsyncCodeActivityContext>> ExecuteAsync(AsyncCodeActivityContext context, CancellationToken cancellationToken)
+        protected override (List<string> Text, int ModelGroup) GetInputs(AsyncCodeActivityContext ctx) => (Text.Get(ctx), ModelGroup.Get(ctx));
+
+        protected override async Task<(List<string> TopResults, List<Dictionary<string, double>> Results)> ExecuteAsync((List<string> Text, int ModelGroup) input, CancellationToken cancellationToken)
         {
-            // Inputs
-            var timeout = TimeoutMS.Get(context);
+            var classifyResults = await Application.Classify(input.Text, input.ModelGroup, cancellationToken);
 
-            // Set a timeout on the execution
-            var task = ExecuteWithTimeout(context, cancellationToken);
-            if (await Task.WhenAny(task, Task.Delay(timeout, cancellationToken)) != task) throw new TimeoutException(Resources.Timeout_Error);
+            return (GetTopResults(classifyResults).ToList(), classifyResults);
+        }
 
-            // Find the top class for each result set
-            List<String> topResults = new List<String>();
-            foreach (Dictionary<String, Double> resultSet in task.Result)
+        private IEnumerable<string> GetTopResults(List<Dictionary<string, double>> classifyResults)
+        {
+            foreach (var resultSet in classifyResults)
             {
-                KeyValuePair<String, Double> topClass = new KeyValuePair<String, Double>();
-                foreach (KeyValuePair<String, Double> result in resultSet)
+                var topClass = new KeyValuePair<string, double>();
+                foreach (var result in resultSet)
                 {
                     if (result.Value > topClass.Value)
                     {
                         topClass = result;
                     }
                 }
-                topResults.Add(topClass.Key);
+
+                yield return topClass.Key;
             }
-
-            // Outputs
-            return async (ctx) =>
-            {
-                TopResults.Set(ctx, topResults);
-                Results.Set(ctx, await task);
-            };
         }
 
-        private async Task<List<Dictionary<string, double>>> ExecuteWithTimeout(AsyncCodeActivityContext context, CancellationToken cancellationToken = default)
+        protected override void SetOutputs(AsyncCodeActivityContext ctx, (List<string> TopResults, List<Dictionary<string, double>> Results) output)
         {
-            var text = Text.Get(context);
-            var modelgroup = ModelGroup.Get(context);
-
-            var objectContainer = context.GetFromContext<IObjectContainer>(IndicoScope.ParentContainerPropertyTag);
-            var application = objectContainer.Get<Application>();
-            var result = await application.Classify(text, modelgroup, cancellationToken);
-            return result;
+            TopResults.Set(ctx, TopResults);
+            Results.Set(ctx, Results);
         }
-
-        #endregion
     }
 }
-
