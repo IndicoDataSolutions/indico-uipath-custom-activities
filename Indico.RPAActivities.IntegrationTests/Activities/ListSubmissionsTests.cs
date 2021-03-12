e809a2087501722baa3fe0cf641ee296493ac1f5
Rafal Kaczka <rafalkaczka@venturedevs.com>
2021-03-12 15:29:48 +0100

    INT-64 add ListSubmissions test

---
+++ Indico.RPAActivities.IntegrationTests/Activities/ListSubmissionsTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Indico.RPAActivities.Activities;
+using Indico.RPAActivities.IntegrationTests.Helpers;
+using IndicoV2.Submissions.Models;
+using NUnit.Framework;
+
+namespace Indico.RPAActivities.IntegrationTests.Activities
+{
+    public class ListSubmissionsTests
+    {
+        [Test]
+        public void ListSubmissions_ReturnsSubmissions()
+        {
+            var action = new ListSubmissions
+            {
+                Limit = 1
+            };
+
+            var result = action.Invoke<ListSubmissions, (List<int> WorkflowIds, List<int> SubmissionIds, SubmissionFilter Filters, int Limit), List<ISubmission>>(
+                (a, outArg) => a.Submissions = outArg);
+
+            result.Count.Should().Be(1);
+            var submission = result.Single();
+            submission.DatasetId.Should().BeGreaterThan(0);
+            submission.WorkflowId.Should().BeGreaterThan(0);
+            submission.Id.Should().BeGreaterThan(0);
+            submission.InputFile.Should().NotBeEmpty();
+        }
+    }
+}
