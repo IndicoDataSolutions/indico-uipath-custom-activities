56cc33c88b09e5cd6d1911369982cf76135851a8
Szymon Socha <szymon.socha@venturedevs.com>
2021-03-30 12:58:34 +0200

    INT-106 Added new categories to the resources and assigned them to the activities.

--- Indico.RPAActivities/Indico.RPAActivities.Activities.Design/DesignerMetadata.cs
+++ Indico.RPAActivities/Indico.RPAActivities.Activities.Design/DesignerMetadata.cs
@@ -13,45 +13,49 @@ namespace Indico.RPAActivities.Activities.Design
             var builder = new AttributeTableBuilder();
             builder.ValidateTable();
 
-            var categoryAttribute = new CategoryAttribute($"{Resources.Category}");
+            var rootCategoryAttribute = new CategoryAttribute($"{Resources.RootCategory}");
+            var workflowCategoryAttribute = new CategoryAttribute($"{Resources.WorkflowCategory}");
+            var ocrCategoryAttribute = new CategoryAttribute($"{Resources.OCRCategory}");
+            var submissionCategoryAttribute = new CategoryAttribute($"{Resources.SubmissionCategory}");
+            var preparationCategoryAttribute = new CategoryAttribute($"{Resources.PreparationCategory}");
 
-            builder.AddCustomAttributes(typeof(IndicoScope), categoryAttribute);
+            builder.AddCustomAttributes(typeof(IndicoScope), rootCategoryAttribute);
             builder.AddCustomAttributes(typeof(IndicoScope), new DesignerAttribute(typeof(IndicoScopeDesigner)));
             builder.AddCustomAttributes(typeof(IndicoScope), new HelpKeywordAttribute(""));
 
-            builder.AddCustomAttributes(typeof(ListDatasets), categoryAttribute);
+            builder.AddCustomAttributes(typeof(ListDatasets), preparationCategoryAttribute);
             builder.AddCustomAttributes(typeof(ListDatasets), new DesignerAttribute(typeof(ListDatasetsDesigner)));
             builder.AddCustomAttributes(typeof(ListDatasets), new HelpKeywordAttribute(""));
 
-            builder.AddCustomAttributes(typeof(GetModelGroup), categoryAttribute);
+            builder.AddCustomAttributes(typeof(GetModelGroup), preparationCategoryAttribute);
             builder.AddCustomAttributes(typeof(GetModelGroup), new DesignerAttribute(typeof(GetModelGroupDesigner)));
             builder.AddCustomAttributes(typeof(GetModelGroup), new HelpKeywordAttribute(""));
 
-            builder.AddCustomAttributes(typeof(DocumentExtraction), categoryAttribute);
+            builder.AddCustomAttributes(typeof(DocumentExtraction), ocrCategoryAttribute);
             builder.AddCustomAttributes(typeof(DocumentExtraction), new DesignerAttribute(typeof(DocumentExtractionDesigner)));
             builder.AddCustomAttributes(typeof(DocumentExtraction), new HelpKeywordAttribute(""));
 
-            builder.AddCustomAttributes(typeof(Classify), categoryAttribute);
+            builder.AddCustomAttributes(typeof(Classify), preparationCategoryAttribute);
             builder.AddCustomAttributes(typeof(Classify), new DesignerAttribute(typeof(ClassifyDesigner)));
             builder.AddCustomAttributes(typeof(Classify), new HelpKeywordAttribute(""));
 
-            builder.AddCustomAttributes(typeof(ListWorkflows), categoryAttribute);
+            builder.AddCustomAttributes(typeof(ListWorkflows), workflowCategoryAttribute);
             builder.AddCustomAttributes(typeof(ListWorkflows), new DesignerAttribute(typeof(ListWorkflowsDesigner)));
             builder.AddCustomAttributes(typeof(ListWorkflows), new HelpKeywordAttribute(""));
 
-            builder.AddCustomAttributes(typeof(WorkflowSubmission), categoryAttribute);
+            builder.AddCustomAttributes(typeof(WorkflowSubmission), workflowCategoryAttribute);
             builder.AddCustomAttributes(typeof(WorkflowSubmission), new DesignerAttribute(typeof(WorkflowSubmissionDesigner)));
             builder.AddCustomAttributes(typeof(WorkflowSubmission), new HelpKeywordAttribute(""));
 
-            builder.AddCustomAttributes(typeof(SubmissionResult), categoryAttribute);
+            builder.AddCustomAttributes(typeof(SubmissionResult), submissionCategoryAttribute);
             builder.AddCustomAttributes(typeof(SubmissionResult), new DesignerAttribute(typeof(SubmissionResultDesigner)));
             builder.AddCustomAttributes(typeof(SubmissionResult), new HelpKeywordAttribute(""));
 
-            builder.AddCustomAttributes(typeof(ListSubmissions), categoryAttribute);
+            builder.AddCustomAttributes(typeof(ListSubmissions), submissionCategoryAttribute);
             builder.AddCustomAttributes(typeof(ListSubmissions), new DesignerAttribute(typeof(ListSubmissionsDesigner)));
             builder.AddCustomAttributes(typeof(ListSubmissions), new HelpKeywordAttribute(""));
 
-            builder.AddCustomAttributes(typeof(SubmitReview), categoryAttribute);
+            builder.AddCustomAttributes(typeof(SubmitReview), submissionCategoryAttribute);
             builder.AddCustomAttributes(typeof(SubmitReview), new DesignerAttribute(typeof(SubmitReviewDesigner)));
             builder.AddCustomAttributes(typeof(SubmitReview), new HelpKeywordAttribute(""));
 
